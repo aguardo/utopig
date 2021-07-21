204f4185b6cab66dcab90c02a8f37628e199881d
LlorensUtopig <llorens@utopigstudio.com>
2021-07-21 15:42:50 +0200

    Add delete button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,9 +7,12 @@
         <div class="card" style="width: 18rem;">
             <img src="https://media.vandal.net/m/43030/the-legend-of-zelda-breath-of-the-wild-201732131429_1.jpg" class="card-img-top" alt="...">
             <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <div class="h-100 d-flex flex-row justify-content-between">
+                    <a href="#" class="btn btn-primary">View</a>
+                    <button type="button" class="btn btn-danger position-end">Delete</button>
+                </div>
             </div>
         </div>
     </div>
@@ -18,9 +21,12 @@
         <div class="card" style="width: 18rem;">
             <img src="https://media.vandal.net/m/43395/super-mario-odyssey-2017102611035_25.jpg" class="card-img-top" alt="...">
             <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <div class="h-100 d-flex flex-row justify-content-between">
+                    <a href="#" class="btn btn-primary">View</a>
+                    <button type="button" class="btn btn-danger position-end">Delete</button>
+                </div>
             </div>
         </div>
     </div>
@@ -29,9 +35,12 @@
         <div class="card" style="width: 18rem;">
             <img src="https://media.vandal.net/m/61486/pokemon-espada-y-escudo-201972612334165_18.jpg" class="card-img-top" alt="...">
             <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <div class="h-100 d-flex flex-row justify-content-between">
+                    <a href="#" class="btn btn-primary">View</a>
+                    <button type="button" class="btn btn-danger position-end">Delete</button>
+                </div>
             </div>
         </div>
     </div>
@@ -40,9 +49,12 @@
         <div class="card" style="width: 18rem;">
             <img src="https://media.vandal.net/m/70568/the-legend-of-zelda-links-awakening-2019612813232_1.jpg" class="card-img-top" alt="...">
             <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <div class="h-100 d-flex flex-row justify-content-between">
+                    <a href="#" class="btn btn-primary">View</a>
+                    <button type="button" class="btn btn-danger position-end">Delete</button>
+                </div>
             </div>
         </div>
     </div>
@@ -51,9 +63,12 @@
         <div class="card" style="width: 18rem;">
             <img src="https://media.vandal.net/m/102188/mario-rabbids-sparks-of-hope-20216139202015_1.jpg" class="card-img-top" alt="...">
             <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <div class="h-100 d-flex flex-row justify-content-between">
+                    <a href="#" class="btn btn-primary">View</a>
+                    <button type="button" class="btn btn-danger position-end">Delete</button>
+                </div>
             </div>
         </div>
     </div>
