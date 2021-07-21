3e87b9fc9658e61205bf74d88d82ae7d2398ec77
LlorensUtopig <llorens@utopigstudio.com>
2021-07-21 13:19:48 +0200

    Refactor blades

---
+++ resources/views/game-detail.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+    <h2>GAME DETAIL</h2>
+@stop
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,75 +1,64 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout')
 
-        <title>UTOPIG GAMES</title>
+@section('content')
+<div class="col d-flex justify-content-center mt-5">
+    <h1>WELCOME TO UTOPIG GAMES</h1>
+</div>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    </head>
-    <body>
-        <div class="col d-flex justify-content-center mt-5">
-            <h1>WELCOME TO UTOPIG GAMES</h1>
-        </div>
-
-        <div class="row justify-content-center mt-5">
-            <div class="col-md-2">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://media.vandal.net/m/43030/the-legend-of-zelda-breath-of-the-wild-201732131429_1.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-2">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://media.vandal.net/m/43395/super-mario-odyssey-2017102611035_25.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-2">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://media.vandal.net/m/61486/pokemon-espada-y-escudo-201972612334165_18.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-2">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://media.vandal.net/m/70568/the-legend-of-zelda-links-awakening-2019612813232_1.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-2">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://media.vandal.net/m/102188/mario-rabbids-sparks-of-hope-20216139202015_1.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
+<div class="row justify-content-center mt-5">
+    <div class="col-md-2">
+        <div class="card" style="width: 18rem;">
+            <img src="https://media.vandal.net/m/43030/the-legend-of-zelda-breath-of-the-wild-201732131429_1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+
+    <div class="col-md-2">
+        <div class="card" style="width: 18rem;">
+            <img src="https://media.vandal.net/m/43395/super-mario-odyssey-2017102611035_25.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-2">
+        <div class="card" style="width: 18rem;">
+            <img src="https://media.vandal.net/m/61486/pokemon-espada-y-escudo-201972612334165_18.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-2">
+        <div class="card" style="width: 18rem;">
+            <img src="https://media.vandal.net/m/70568/the-legend-of-zelda-links-awakening-2019612813232_1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-2">
+        <div class="card" style="width: 18rem;">
+            <img src="https://media.vandal.net/m/102188/mario-rabbids-sparks-of-hope-20216139202015_1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+</div>
+@stop
