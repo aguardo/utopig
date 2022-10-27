21c9060755eeee91a1dd0c942e746bef5138fa3e
Willi Maese <willimaese@gmail.com>
2022-10-27 14:06:28 +0200

    update views

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,12 +7,11 @@
         <title>UTOPIG GAMES</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     </head>
     <body>
-        <div class="col d-flex justify-content-center mt-5">
-            <h1>UTOPIG GAMES</h1>
+        <div class="col d-flex justify-content-center my-5">
+            <h1>utopigGames</h1>
         </div>
 
         @yield('content')
